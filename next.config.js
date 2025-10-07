54bfedc357b940bcf0e2968cb9dc55d2ad1e7452
Ripon Chandra Saha <riponsaha199@gmail.com>
2025-10-07 12:47:48 +0600

    commit with my code

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com", // for GitHub images
+      },
+    ],
+  },
+}
+
+export default nextConfig
